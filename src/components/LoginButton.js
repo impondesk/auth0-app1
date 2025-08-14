6b0d2880dc5b410c2257868f3bb0185d96ce7507
Israni, Murli <murli.p.israni@gmail.com>
2025-08-14 09:03:42 +0530

    Refactor LoginButton component to include redirect URIs for login

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,7 +4,19 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect({})}>Log In</button>;
+  return (
+    <button
+      onClick={() =>
+        loginWithRedirect({
+          redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+          post_logout_redirect_uri:
+            process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI,
+        })
+      }
+    >
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
