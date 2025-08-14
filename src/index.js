5b00cab0d69c1c9205776c45c441690b727a582a
Israni, Murli <murli.p.israni@gmail.com>
2025-08-14 08:26:34 +0530

    Add environment variables for Auth0 configuration and update index.js to use them

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="misrani.auth0.com"
-      clientId="GhZosdvUUv2Xef5XVhfjqeF9OaitEr0T"
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
