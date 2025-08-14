b2094ca724ac288f07db4dab26b01586fe435d6b
Israni, Murli <murli.p.israni@gmail.com>
2025-08-14 09:09:53 +0530

    Add logging for environment variables in LoginButton component

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,6 +4,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
+  console.log("LoginButton component rendered");
+  console.log(process.env.REACT_APP_AUTH0_DOMAIN);
+  console.log(process.env.REACT_APP_REDIRECT_URI);
+  console.log(process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI);
+
   return (
     <button
       onClick={() =>
